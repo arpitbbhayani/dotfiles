56df8c6bbe25e235c029e92201ae7400b7ad21db
Arpit <arpit.b.bhayani@gmail.com>
2025-01-17 13:30:58 +0000

    Configuring hostname

--- setup.sh
+++ setup.sh
@@ -2,6 +2,10 @@
 sudo apt-get update
 sudo apt-get install -y git curl
 
+if hostnamectl | grep -q "speedster"; then
+    sudo hostnamectl set-hostname speedster
+fi
+
 # install zsh only when not installed
 if ! command -v zsh &> /dev/null; then
     sh install_zsh.sh
