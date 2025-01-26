f852fb75a116daa8cb78588f488f9010d4e56612
Arpit <arpit.b.bhayani@gmail.com>
2025-01-26 12:02:32 +0000

    Protobuf installations

--- install_go.sh
+++ install_go.sh
@@ -13,3 +13,6 @@ echo 'export GOBIN=$GOPATH/bin' >> ~/.bashrc
 echo 'export PATH=$PATH:$GOBIN' >> ~/.bashrc
 
 sudo curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sudo sh -s -- -b /bin v1.60.1
+
+sudo apt-get install -y protobuf-compiler
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
