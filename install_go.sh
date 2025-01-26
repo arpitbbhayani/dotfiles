1709e3adb7da8697834c52539607be2b460a5d67
Arpit <arpit.b.bhayani@gmail.com>
2025-01-26 13:26:24 +0000

    Installing protoc-gen-go-grpc

--- install_go.sh
+++ install_go.sh
@@ -16,3 +16,4 @@ sudo curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/
 
 sudo apt-get install -y protobuf-compiler
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
