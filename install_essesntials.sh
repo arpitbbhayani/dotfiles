daee668b94f892d089faa8baa12f6c0b4f99555f
Arpit <arpit.b.bhayani@gmail.com>
2025-03-27 18:40:20 +0000

    AWS CLI

--- install_essesntials.sh
+++ install_essesntials.sh
@@ -1,2 +1,3 @@
 sudo apt-get install -y make cmake
 sudo apt-get install -y clang-tidy clang-format
+sudo apt-get install -y unzip
--- install_go.sh
+++ install_go.sh
@@ -1,4 +1,4 @@
-INSTALL_GO_VERSION="1.24.0"
+INSTALL_GO_VERSION="1.24.1"
 
 if command -v go &> /dev/null && go version | grep -q "go$INSTALL_GO_VERSION"; then
     exit 0
