66be1c447304f52d24f1d2f8353f56192c20a5f2
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2026-03-24 17:16:24 +0000

    Lazyvim

--- install_neovim.sh
+++ install_neovim.sh
@@ -3,7 +3,8 @@ sudo rm -rf /opt/nvim-linux-x86_64
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 
 if ! grep -q "/opt/nvim-linux-x86_64/bin" ~/.bashrc; then
-    echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.bashrc
+  echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >>~/.bashrc
 fi
 
-rm nvim-linux-x86_64.tar.gz
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git nvim-linux-x86_64.tar.gz
